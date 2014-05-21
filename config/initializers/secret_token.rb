4e23b803e21e4f60ba83d73b145d97858524aecc
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-20 18:40:07 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Phlickr::Application.config.secret_key_base = '450b649b7d8438571e74ee7171b20aed60b85cb269a5139ee521200587015c6e601c2ede16059ed264da60e654b465ecbb92f22846e09be4b9c1d728136dc279'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Phlickr::Application.config.session_store :cookie_store, key: '_phlickr_session'
