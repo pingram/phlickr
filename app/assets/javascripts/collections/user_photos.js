ad2a66d42228d08ec15856a4fc801f746b047ef0
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-22 10:25:52 -0700

    user profile pulls in pictures based on url

---
+++ app/assets/javascripts/collections/user_photos.js
@@ -0,0 +1,10 @@
+Phlickr.Collections.UserPhotos = Backbone.Collection.extend({
+  model: Phlickr.Models.Photo,
+  url: function() {
+    return 'api/users/' + this.user_id + '/photos'
+  },
+  
+  initialize: function (models, options) {
+    this.user_id = options.user_id;
+  }
+})
--- app/assets/javascripts/views/users/profile.js
+++ app/assets/javascripts/views/users/profile.js
@@ -1,6 +1,12 @@
 Phlickr.Views.UserProfile = Backbone.View.extend({
   initialize: function (options) {
     this.model = options.model
+    this.collection = new Phlickr.Collections.UserPhotos([], {
+      user_id: options.model.id
+    })
+
+    this.collection.fetch();
+    this.listenTo(this.collection, 'sync', this.render);
   },
 
   template: JST['users/profile'],
@@ -8,7 +14,8 @@ Phlickr.Views.UserProfile = Backbone.View.extend({
   render: function() {
     console.log('render users/profile')
     var renderedContent = this.template({
-      user: this.model
+      user: this.model,
+      photos: this.collection
     });
     this.$el.html(renderedContent);
     return this;
