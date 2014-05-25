913da198f59e830853c635ba66bc0495bd443436
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-25 10:56:22 -0700

    update seed data to use Amazon S3"

--- app/views/api/albums/_album.jbuilder
+++ app/views/api/albums/_album.jbuilder
@@ -3,4 +3,4 @@ json.user_id      album.user_id
 json.name         album.name
 json.description  album.description
 
-json.photos @album.photos.order(:id), partial: 'api/photos/photo', as: :photo
+json.photos @album.photos, partial: 'api/photos/photo', as: :photo
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}.jpg"))
+end
+
 guest = User.create!(username: 'guest', password_digest: 'n/a', fname: 'Guest', lname: 'User')
 User.create!(username: 'user2', password_digest: 'n/a', fname: 'User2', lname: 'U2lname')
 
 album1 = Album.create!(user_id: guest.id, name: 'My first album!', description: 'album description...')
 album2 = Album.create!(user_id: guest.id, name: 'My second album!', description: 'album description2...')
 
-photo_urls = [
-  "http://fc09.deviantart.net/fs71/i/2012/230/b/e/grassy_field_sunset___free_stock_by_kevron2001-d5blgkr.jpg",
-  "http://www.techiemania.com/wp-content/uploads/2010/09/freebie-images-free-stock-images-plugin.jpg",
-  "http://ethicsalarms.files.wordpress.com/2012/03/beautiful-2ddrawings-2d41.jpg",
-  "http://www.wallsave.com/wallpapers/1024x640/smooking/34363/smooking-free-stock-p-os-images-part-34363.jpg",
-  "http://static1.businessinsider.com/image/50185063eab8ea6802000002/apple-ceo-tim-cook-likes-these-7-beautiful-things.jpg",
-]
+
+photo_urls = []
+seed_image_path = Rails.root.join("app/assets/images/seed")
+general_seed_path = seed_image_path.join("general")
+Dir.entries(general_seed_path).each do |item|
+  next if item == '.' or item == '..'
+  photo_urls << "#{general_seed_path + item}"
+end
 
 photo_urls.each_with_index do |photo_url, i|
   new_photo = album1.photos.build(
     user_id: 1,
     description: "stock photo number #{i + 1}",
-    url: photo_url)
+    file: File.open(photo_url))
   new_photo.save!
 end
 
