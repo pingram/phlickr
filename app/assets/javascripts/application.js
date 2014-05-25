a9e2034e2cdbac9d221524479ec98dcde714c103
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-24 18:20:39 -0700

    add dragging to album new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.sortable
 //= require underscore
 //= require serialize_json
 //= require backbone
--- app/assets/javascripts/views/albums/new.js
+++ app/assets/javascripts/views/albums/new.js
@@ -9,12 +9,33 @@ Phlickr.Views.AlbumNew = Backbone.View.extend({
     this.listenTo(this.userPhotos, "sync", this.render);
   },
 
+  events: {
+    'click .cancel': 'cancel',
+    'click .save': 'save'
+  },
+
   render: function () {
     var renderedContent = this.template({
       userPhotos: this.userPhotos,
       albumPhotos: this.albumPhotos
     });
     this.$el.html(renderedContent);
+
+    $( "#sortable1, #sortable2" ).sortable({
+      connectWith: ".connected-sortable",
+      items: "li"
+    }).disableSelection();
+
     return this;
+  },
+
+  cancel: function (event) {
+    event.preventDefault();
+    Backbone.history.history.back();
+  },
+
+  save: function (event) {
+    event.preventDefault();
+    debugger
   }
 })
