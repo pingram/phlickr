c869b8d00e2370e98946fd6ed87214a8f0b97bcf
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-27 09:39:55 -0700

    new file upload working, no progress bar or information

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Phlickr::Application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :photos, only: :create
 
-  # namespace :api do
-  #   resources :photos, only: [:new]
-  # end
+  namespace :api do
+    resources :photos, only: [:new]
+  end
 
   namespace :api, defaults: { format: :json } do
     get "users/profile", to: "users#profile"
