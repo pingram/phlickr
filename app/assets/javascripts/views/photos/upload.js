ca672fe6db802bcf6244fee42ca3a3ce4e0f94b2
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-28 10:37:02 -0700

    file uploader working without progress bar

--- app/assets/javascripts/views/photos/upload.js
+++ app/assets/javascripts/views/photos/upload.js
@@ -7,15 +7,51 @@ Phlickr.Views.PhotoUpload = Backbone.View.extend({
   },
 
   events: {
-    'submit form' : 'submit',
-    'change input[type=file]' : 'encodeFile'
+    // 'submit form' : 'submit',
+    // 'change input[type=file]' : 'encodeFile'
   },
 
   render: function () {
     console.log('render photo upload');
-    var renderedContent = this.template();
+    var renderedContent = this.template({
+      photo: this.model
+    });
     this.$el.html(renderedContent);
+    this.renderFileUploader();
     return this;
+  },
+
+  renderFileUploader: function() {
+    var photo = this.model;
+    var view = this;
+
+    this.$el.find('#fileupload').fileupload({
+      dataType: 'json',
+      url: 'api/photos',
+      add: function (e, data) {
+        view.encodeFile(e);
+        $("#up_btn").on('click', function () {
+            data.submit();
+        });
+      },
+      // done: function () {debugger},
+      // send: function () {debugger},
+      // start: function (e, data) {
+        // debugger
+        
+      // },
+      submit: function (e, data) {
+        var attrs = $('form').serializeJSON();
+        photo.set(attrs);
+
+        photo.save(null, {
+          success: function (attribute) {
+            console.log("file uploaded successfully!");
+            Backbone.history.navigate('profile', { trigger: true });
+          },
+        });
+      }
+    });
   },
 
   encodeFile: function (event) {
@@ -38,12 +74,13 @@ Phlickr.Views.PhotoUpload = Backbone.View.extend({
 
   submit: function (event) {
     event.preventDefault();
-    $submitBtn = this.$el.find('input.btn');
-    $submitBtn.attr("disabled", "disabled").val('Uploading..');
-    $submitBtn.removeClass('btn-primary').addClass('btn-success');
+    // $submitBtn = this.$el.find('input.btn');
+    // $submitBtn.attr("disabled", "disabled").val('Uploading..');
+    // $submitBtn.removeClass('btn-primary').addClass('btn-success');
     
-    var attrs = $(event.currentTarget).serializeJSON();
+    var attrs = $('form').serializeJSON();
     this.model.set(attrs);
+    debugger
 
     this.model.save(null, {
       success: function (attribute) {
